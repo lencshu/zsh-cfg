3ec76e4cab165c8457c0a86355637a62ad54621f
Guoliang <guoliang.li@assurly.com>
2025-03-09 22:46:46 +0100

    optimised: export PATH

--- z_utils.zsh
+++ z_utils.zsh
@@ -6,4 +6,17 @@ alias tabe='sudo nano /var/at/tabs/vi'
 export CHROME_EXECUTABLE="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 source /Users/vi/assurly/retroApiChatGPT/cred.sh
 
-export PATH="$HOME/bin:$HOME/.amplify/bin:/opt/homebrew/sbin:/opt/homebrew/bin:$HOME/.local/bin:/Library/Frameworks/Python.framework/Versions/3.11/bin:/System/Cryptexes/App/usr/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="
+$HOME/bin
+$HOME/.amplify/bin
+/opt/homebrew/sbin
+/opt/homebrew/bin
+$HOME/.local/bin
+/Library/Frameworks/Python.framework/Versions/3.11/bin
+/System/Cryptexes/App/usr/bin
+/usr/local/bin
+/usr/bin
+/bin
+/usr/sbin
+/sbin
+$PATH"
