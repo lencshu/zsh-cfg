933bd13bc1f357397a29451be636782720ee64cc
lencshu <lencshu@gmail.com>
2025-04-06 15:37:52 +0200

    updated sh scripts

--- z_utils.zsh
+++ z_utils.zsh
@@ -8,6 +8,5 @@ alias ip='interpreter'
 alias tabe='sudo nano /var/at/tabs/vi'
 # export CHROME_EXECUTABLE="/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
 export CHROME_EXECUTABLE="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-source /Users/vi/assurly/retroApiChatGPT/cred.sh
 
 export PATH="$HOME/bin:$HOME/.amplify/bin:/opt/homebrew/sbin:/opt/homebrew/bin:$HOME/.local/bin:/Library/Frameworks/Python.framework/Versions/3.11/bin:/System/Cryptexes/App/usr/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
