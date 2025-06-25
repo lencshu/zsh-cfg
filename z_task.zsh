20696f21b58d10739368792ba3553c8250e372dc
Guoliang <gliang@yampa.ai>
2025-06-25 17:40:07 +0200

    removed duplicate alias definition for tls in z_task.zsh and updated tsb alias to filter output for cleaner logging

--- z_task.zsh
+++ z_task.zsh
@@ -1,6 +1,12 @@
 alias t='task'
 alias tls='task list'
 alias tls='task list'
+
+
+# alias tsb='t app:run 2>&1 | perl -pe "s/\x00//g; s/\e\[[0-9;]*[a-zA-Z]//g" > runb.log'
+
 alias tsb='t app:run > runb.log 2>&1'
+
 alias tsf='cd front && npm install && npm run dev | tee ../runf.log'
+
 alias tsdev='trap "cd ..; exit" INT; tsb & tsf; wait'
