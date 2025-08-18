26f304d56e8e00edbf7f5256faa7fa198824f54e
Guoliang <gliang@yampa.ai>
2025-08-18 16:59:23 +0200

    Added new Python version aliases in z_py.zsh for easier management of Python environments, including 'pg313' for version 3.13.7 and 'py' for python3.13. Updated PYENV_ROOT and initialization for pyenv to streamline Python version switching.

--- z_py.zsh
+++ z_py.zsh
@@ -1,6 +1,11 @@
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+
 alias wg='python /Users/vi/winGo.py'
 alias pg311='pyenv global 3.11 && pyenv shell 3.11 && python --version'
 alias pg39='pyenv global 3.9 && pyenv shell 3.9 && python --version'
+alias pg313='pyenv global 3.13.7 && pyenv shell 3.13.7 && python --version'
 alias pop='poetry run python'
 alias po='poetry'
 alias pth='pytest -vv -W ignore::DeprecationWarning --html=./_logs/report.html --self-contained-html'
@@ -9,3 +14,4 @@ alias pt='pytest -vv -W ignore::DeprecationWarning'
 alias envon='source .venv/bin/activate'
 alias envinit='virtualenv .venv'
 alias pip='pip3'
+alias py='python3.13'
